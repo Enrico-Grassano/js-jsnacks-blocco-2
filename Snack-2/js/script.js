3eee6cb2a9a831c0aec4a3e15cd2580992240025
Enrico-Grassano <grassano.enrico1996@gmail.com>
2021-05-11 17:10:23 +0200

    Array created

---
+++ Snack-2/js/script.js
@@ -0,0 +1,24 @@
+//Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate inizialmente a 0.
+var footballClubs = [
+    club1 = {
+        name: "Juventus F.C.",
+        scores: 0,
+        foulsSuffered: 0
+    },
+    club2 = {
+        name: "Chelsea F.C.",
+        scores: 0,
+        foulsSuffered: 0
+    },
+    club3 = {
+        name: "Manchester United F.C.",
+        scores: 0,
+        foulsSuffered: 0
+    }
+]
+
+
+//Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+//Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
